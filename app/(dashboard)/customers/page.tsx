5ca578472021a522a6f790614b8ba8910bb2147e
MuradRashidov <muradrashidov995@gmail.com>
2024-05-19 17:30:03 +0400

    df

--- app/(dashboard)/customers/page.tsx
+++ app/(dashboard)/customers/page.tsx
@@ -7,14 +7,15 @@ import React from 'react'
 const Customers = async() => {
   const customers = await Customer.find().sort({createdAt:"desc"});
   
-  return  (
+  return customers && customers.length>0? (
 
             <div className='px-10 py-5'>
             <p className='text-heading2-bold'>Customers</p>
             <Separator className='bg-grey-1 my-5' />
             <DataTable columns={columns} data={customers} searchKey='name'/>
             </div>
-          )
+          ):<>There is no customer</>
 }
+export const dynamic = "force-dynamic";
 
 export default Customers
